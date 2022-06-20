a254b660a483953616757669f87e782430304a3e
dune-omega <107744111+dune-omega@users.noreply.github.com>
2022-06-20 11:45:34 +0800

    Update Root.tsx

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Route, useRoutes } from "react-router-dom";
 import Home from "./home";
 import Navbar from "./navbar";
 
 const Root: FC = () => {
+  const [count, setCount] = useState(0)
   return (
     <>
       <Navbar />
